a2259f3acdaeedff966f0b5afb524830f6fb2c48
Krist Menina <krist.menina@me.com>
2025-02-01 11:21:31 +0800

    comments index, new, create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,43 @@
 class CommentsController < ApplicationController
+  def index
+    @post = Post.find_by(params[:post_id])
+    @comments = Comment.where(post_id: @post.id)
+  end
+
+  def new
+    @post = Post.find_by(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new comment_params
+    @post = Post.find_by(params[:post_id])
+    @comment.post_id = @post.id
+
+    if @comment.save
+      redirect_to post_comments_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    @comment = Comment.find_by(params[:id])
+  end
+
+  def delete
+  end
+
+  private
+
+  def comment_params
+    #params.require(:comment).permit(:comment, :author)
+    params.expect(comment: [ :comment, :author ])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    get '/comments' => 'comments#index'
+    get '/comments/new' => 'comments#new'
+    post '/comments' => 'comments#create'
+    get '/comments/:id' => 'comments#show'
+  end
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
